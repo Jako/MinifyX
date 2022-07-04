81e59d6415f8f86ff1222db50c0cc1442bc7cbe0
Thomas Jakobi <buero@treehillstudio.de>
2022-07-04 08:00:01 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (24 of 24 strings)
    
    Translation: MODX Extras/MODX MinifyX/System Settings
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/modx-minifyx-system-settings/de/

--- core/components/minifyx/lexicon/de/setting.inc.php
+++ core/components/minifyx/lexicon/de/setting.inc.php
@@ -5,27 +5,27 @@
  * @package minifyx
  * @subpackage lexicon
  */
-$_lang['setting_minifyx.cacheFolder'] = 'Cache Folder';
-$_lang['setting_minifyx.cacheFolder_desc'] = 'Specify the folder where the plugin will put the results of it’s work. You can specify a non-existent folder, it will be created automatically.';
+$_lang['setting_minifyx.cacheFolder'] = 'Cache-Ordner';
+$_lang['setting_minifyx.cacheFolder_desc'] = 'Geben Sie den Ordner an, in dem das Plugin die Ergebnisse seiner Arbeit ablegt. Sie können einen nicht existierenden Ordner angeben, er wird dann automatisch erstellt.';
 $_lang['setting_minifyx.cacheUrl'] = 'Cache URL';
-$_lang['setting_minifyx.cacheUrl_desc'] = 'Specify the URL where the plugin will put the results of it’s work. It has to point to the cache folder.';
-$_lang['setting_minifyx.cssFilename'] = 'CSS Filename';
-$_lang['setting_minifyx.cssFilename_desc'] = 'Specify the name of the prepared CSS file that will contain all processed scripts. To it will be added the time of creation and suffix .min, if compression is enabled.';
+$_lang['setting_minifyx.cacheUrl_desc'] = 'Geben Sie die URL an, von der das Plugin die Ergebnisse seiner Arbeit abruft. Die URL muss auf den Cache-Ordner verweisen.';
+$_lang['setting_minifyx.cssFilename'] = 'CSS Dateiname';
+$_lang['setting_minifyx.cssFilename_desc'] = 'Geben Sie den Namen der CSS-Datei an, die alle kombinierten Stile enthalten soll. Wenn die Komprimierung aktiviert ist, wird das Suffix .min hinzugefügt.';
 $_lang['setting_minifyx.cssTpl'] = 'CSS Template';
-$_lang['setting_minifyx.cssTpl_desc'] = 'Name of a template chunk for the CSS tag. The placeholder "[[+file]]" must be present.';
+$_lang['setting_minifyx.cssTpl_desc'] = 'Name eines Template-Chunks für das CSS-Tag. Der Platzhalter "[[+file]]" muss vorhanden sein.';
 $_lang['setting_minifyx.debug'] = 'Debug';
 $_lang['setting_minifyx.debug_desc'] = 'Log debug information in the MODX error log.';
-$_lang['setting_minifyx.excludeRegistered'] = 'Exclude Scripts and Styles';
-$_lang['setting_minifyx.excludeRegistered_desc'] = 'A regular expression for exclude files from processing. By default excludes scripts and styles prepared by snippet MinifyX.';
-$_lang['setting_minifyx.jsFilename'] = 'Javascript Filename';
-$_lang['setting_minifyx.jsFilename_desc'] = 'Specify the name of the prepared javascript file that will contain all processed scripts. To it will be added the time of creation and suffix .min, if compression is enabled.';
+$_lang['setting_minifyx.excludeRegistered'] = 'Skripte und Stile ausschließen';
+$_lang['setting_minifyx.excludeRegistered_desc'] = 'Ein regulärer Ausdruck zum Ausschließen von Dateien aus der Verarbeitung.';
+$_lang['setting_minifyx.jsFilename'] = 'Javascript Dateiname';
+$_lang['setting_minifyx.jsFilename_desc'] = 'Geben Sie den Namen der Javascript-Datei an, die alle kombinierten Skripte enthalten soll. Wenn die Komprimierung aktiviert ist, wird die Endung .min hinzugefügt.';
 $_lang['setting_minifyx.jsTpl'] = 'Javascript Template';
-$_lang['setting_minifyx.jsTpl_desc'] = 'Name of a template chunk for the JS tag. Placeholder "[[+file]]" must exists.';
-$_lang['setting_minifyx.minifyCss'] = 'Compress CSS?';
-$_lang['setting_minifyx.minifyCss_desc'] = 'You can enable compression CSS compression. All files that have suffix .min in the name will be skipped.';
-$_lang['setting_minifyx.minifyHtml'] = 'Compress HTML?';
-$_lang['setting_minifyx.minifyHtml_desc'] = 'Compress the page content before output.';
-$_lang['setting_minifyx.minifyJs'] = 'Compress Javascript?';
-$_lang['setting_minifyx.minifyJs_desc'] = 'You can enable compression javascript compression. All files that have suffix .min in the name will be skipped.';
-$_lang['setting_minifyx.processRegistered'] = 'Process Scripts and Styles';
-$_lang['setting_minifyx.processRegistered_desc'] = 'You can enable automatic processing of all registered scripts and styles of the page using the plugin MinifyX.';
+$_lang['setting_minifyx.jsTpl_desc'] = 'Name eines Template Chunk für das JS-Tag. Der Platzhalter "[[+file]]" muss vorhanden sein.';
+$_lang['setting_minifyx.minifyCss'] = 'CSS komprimieren';
+$_lang['setting_minifyx.minifyCss_desc'] = 'Aktivieren Sie die CSS-Komprimierung.';
+$_lang['setting_minifyx.minifyHtml'] = 'HTML komprimieren';
+$_lang['setting_minifyx.minifyHtml_desc'] = 'Komprimiert den Seiteninhalt vor der Ausgabe.';
+$_lang['setting_minifyx.minifyJs'] = 'Javascript komprimieren';
+$_lang['setting_minifyx.minifyJs_desc'] = 'Javascript-Komprimierung aktivieren.';
+$_lang['setting_minifyx.processRegistered'] = 'Registrierte verarbeiten';
+$_lang['setting_minifyx.processRegistered_desc'] = 'Sie können die automatische Verarbeitung aller registrierten Skripte und Stile der Seite mithilfe des MinifyX-Plugins aktivieren.';
